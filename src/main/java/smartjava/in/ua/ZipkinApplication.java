e40ffe3dab31cb5055bccb482b30ee2ac0737d89
Roman Tsypuk <rtsypuk@lohika.com>
2017-01-25 16:29:03 +0200

    init commit

---
+++ src/main/java/smartjava/in/ua/ZipkinApplication.java
@@ -0,0 +1,12 @@
+package smartjava.in.ua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+}
